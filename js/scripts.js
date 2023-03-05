4d565a68c6a378d9a7c7801c1860ea057787b6f3
josephinelo1 <josephine.jfenlo@gmail.com>
2023-03-04 16:07:14 -0800

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 console.log("Hi! Welcome to My Portfolio Site")
+var text2 = "Hello, My name is Josephine!"
+
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive'; 
+}
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+
